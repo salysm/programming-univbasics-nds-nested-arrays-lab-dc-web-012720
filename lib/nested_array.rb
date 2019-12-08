9370e36764821d4b8363880ac00f7408d76b9e71
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 09:36:38 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,7 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-assembled_matrix = [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+assembled_matrix([ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY])
 puts assembled_matrix
 end
 
